95293475d6544cec9365820196c148f64cfd2d04
Fabiola Li Wu <fabiliwu@hotmail.com>
2024-11-28 18:18:10 -0500

    LASTTTTT

--- FileTrie.hpp
+++ FileTrie.hpp
@@ -18,7 +18,7 @@ struct FileTrieNode {
     char stored;
     std::unordered_set<File*> matching;
     std::unordered_map<char, FileTrieNode*> next;
-    // std::vector<FileTrieNode*> vector;
+    //std::vector<FileTrieNode*> next;
     FileTrieNode(const char& c = ' ', File* to_add = nullptr) : stored{c}, matching{}, next{} {
         if (to_add) { matching.insert(to_add); }
     }
