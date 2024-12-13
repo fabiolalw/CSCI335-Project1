aa00bf2b422772889dbcd4c3c91bc1b5e0fc02d7
Fabiola Li Wu <fabiliwu@hotmail.com>
2024-12-13 13:02:25 -0500

    sacas

--- TSP.cpp
+++ TSP.cpp
@@ -63,9 +63,22 @@ TSP::Tour TSP::nearestNeighbor(std::list<Node> cities, const size_t& start_id){
       notVisited.push_back(city);
   }
 
-  //find the nearest city
-for(std::vector<Node>::iterator it = notVisited.begin(); it != notVisited.end(); it++){
-  //while(!notVisited.empty()){
+  // find the nearest ity 
+  // while(!notVisited.empty()){
+  //   Node nearest = notVisited.front();
+  //   size_t minDistance = nearest.distance(tour.path.back());
+  //   for(auto& city : notVisited){
+  //     if(city.distance(nearest) < minDistance){
+  //       nearest = city;
+  //       minDistance = city.distance(nearest);
+  //     }
+  //   }
+  //   tour.path.push_back(nearest);
+  //   tour.weights.push_back(minDistance);
+  //   tour.total_distance += minDistance;
+  //   notVisited.erase(std::find(notVisited.begin(), notVisited.end(), nearest));
+  // }
+  for(std::vector<Node>::iterator it = notVisited.begin(); it != notVisited.end(); it++){
     Node nearest = notVisited.front();
     size_t minDistance = nearest.distance(tour.path.back());
     for(auto& city : notVisited){
