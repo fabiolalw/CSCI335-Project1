16f5bd983b84d59389b2be89c8ef3bd7d08ccbc5
Fabiola Li Wu <fabiliwu@hotmail.com>
2024-12-13 02:50:48 -0500

    dsds

--- TSP.cpp
+++ TSP.cpp
@@ -51,12 +51,18 @@ TSP::Tour TSP::nearestNeighbor(std::list<Node> cities, const size_t& start_id){
   //std::list<Node> notVisitedCities = cities;
   
   // find the start city and start the tour
-  for(auto& city : cities){
-    if(city.id == start_id){
-      tour.path.push_back(city);
-      tour.weights.push_back(0);
-      tour.total_distance = 0;
-      cities.remove(city);
+  // for(auto& city : cities){
+  //   if(city.id == start_id){
+  //     tour.path.push_back(city);
+  //     tour.weights.push_back(0);
+  //     tour.total_distance = 0;
+  //     cities.remove(city);
+  //   }
+  // }
+  Node currentCity(0,0,0);
+  for (auto& city : cities) {
+    if (city.id == start_id) {
+        currentCity = city;
     }
   }
     // Node currentCity = *std::find_if(cities.begin(), cities.end(), [start_id](const Node& city) {
