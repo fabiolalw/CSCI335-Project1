5727e4995c5c809868267eb402cea525c1328e59
Fabiola Li Wu <fabiliwu@hotmail.com>
2024-12-13 12:38:15 -0500

    dsds

--- TSP.cpp
+++ TSP.cpp
@@ -81,17 +81,17 @@ TSP::Tour TSP::nearestNeighbor(std::list<Node> cities, const size_t& start_id){
   for(std::vector<Node>::iterator it = notVisited.begin(); it != notVisited.end(); it++){
     Node nearest = notVisited.front();
     size_t minDistance = nearest.distance(tour.path.back());
-    for(auto& city : notVisited){
-      if(city.distance(nearest) < minDistance){
-        nearest = city;
-        minDistance = city.distance(nearest);
-      }
-    }
-    tour.path.push_back(nearest);
-    tour.weights.push_back(minDistance);
-    tour.total_distance += minDistance;
-    notVisited.erase(std::find(notVisited.begin(), notVisited.end(), nearest));
-    it = notVisited.begin();
+    // for(auto& city : notVisited){
+    //   if(city.distance(nearest) < minDistance){
+    //     nearest = city;
+    //     minDistance = city.distance(nearest);
+    //   }
+    // }
+    // tour.path.push_back(nearest);
+    // tour.weights.push_back(minDistance);
+    // tour.total_distance += minDistance;
+    // notVisited.erase(std::find(notVisited.begin(), notVisited.end(), nearest));
+    // it = notVisited.begin();
   }
 
   // //return to the start city
